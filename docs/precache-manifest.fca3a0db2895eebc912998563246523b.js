4f55b6bdc520e80fb85993fe74040b03500de97a
rainyGLC <2929939024@qq.com>
2019-05-25 22:12:17 +0800

    init

---
+++ docs/precache-manifest.fca3a0db2895eebc912998563246523b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "f444f1d6382baceec069",
+    "url": "js/chunk-vendors.0937dee2.js"
+  },
+  {
+    "revision": "8ff6972479b82ff92836",
+    "url": "js/app.4d905d76.js"
+  },
+  {
+    "revision": "041de0577aaae529dd75ba00f100e59f",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "f444f1d6382baceec069",
+    "url": "css/chunk-vendors.af67b8ba.css"
+  },
+  {
+    "revision": "8ff6972479b82ff92836",
+    "url": "css/app.ff24e0a5.css"
+  }
+];
